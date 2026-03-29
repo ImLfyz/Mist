4062140f9a43eefd49cb10a41edc8faca4e6d9e0
L-fyz <thereallfyz@gmail.com>
2026-03-29 00:02:48 +0000

    Long modegit add .
    q
    stop

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Mist
-Light x86_64 OS
+Simple x86_64 operation system
+
+## Attention
+DO NOT start this on real PC. It made by junior for quemu
+
+## Get started
+1. Clone this to your PC:
+```
+git clone https://github.com/L-fyz/Mist.git ~/Mist
+```
+2. Compile this:
+```
+cd ~/Mist && make
+```
+3. Start with qemu:
+```
+qemu-system-x86_64 -drive format=raw,file=os.img -d int,cpu_reset -no-reboot
+```
+
+## Todo:
+1. Keyboard driver
+2. Command line
+3. Command line parsing
+4. Some utils
+5. (Maybe) VGA driver
--- boot/boot.asm
+++ boot/boot.asm
@@ -12,7 +12,6 @@ start:
     mov ss, ax
     mov sp, 0x7C00
 
-    ; A20
     in al, 0x92
     or al, 2
     out 0x92, al
@@ -30,13 +29,54 @@ pm_entry:
     mov ds, ax
     mov es, ax
     mov ss, ax
-    mov esp, 0x9000
+    mov esp, 0x90000
 
     mov edi, 0xB8000
     mov ecx, 2000
     mov ax, 0x0720
     cld
     rep stosw
+
+    mov edi, 0x1000
+    mov cr3, edi
+    
+    mov dword [edi], 0x2003
+    mov dword [edi + 4], 0
+    
+    mov edi, 0x2000
+    mov ebx, 0x00000083
+    mov ecx, 512
+.pd_loop:
+    mov dword [edi], ebx
+    mov dword [edi + 4], 0
+    add ebx, 0x200000
+    add edi, 8
+    loop .pd_loop
+
+    mov byte [gdt_start + 14], 00100000b
+
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+
+    jmp 0x08:lm_entry
+
+bits 64
+lm_entry:
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov rsp, 0x90000
 
     mov esi, welcome_msg
     mov edi, 0xB8000
@@ -64,14 +104,14 @@ gdt_code:
     dw 0
     db 0
     db 10011010b
-    db 11001111b
+    db 01001111b
     db 0
 gdt_
     dw 0xFFFF
     dw 0
     db 0
     db 10010010b
-    db 11001111b
+    db 00000000b
     db 0
 gdt_end:
 
